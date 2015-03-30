36c818d79080c375bc6988d1bbba3874af6684c4
Toni Lähdekorpi <toni@lygon.net>
2015-03-30 13:03:29 +0300

    Fix multibranch

--- publisher.sh
+++ publisher.sh
@@ -2,7 +2,7 @@
 export PATH=$PATH:/usr/local/bin/
 cd /opt/trailcup/trailcup/
 
-if [ `git log --pretty=%H ...refs/heads/master^` != `git ls-remote origin -h refs/heads/master |cut -f1` ]
+if [ `git log --pretty=%H ...refs/heads/master^ | tail -n1` != `git ls-remote origin -h refs/heads/master |cut -f1` ]
 then
 	git pull
 	/usr/local/bin/bundle exec rake build
